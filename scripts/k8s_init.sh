6122fb2ae5b5bd13d58ff4d54048db286fa849d1
mstrielnikov <makstr@ukr.net>
2022-04-23 23:17:30 +0300

    Installed monitoring stack

--- scripts/k8s_init.sh
+++ scripts/k8s_init.sh
@@ -80,4 +80,90 @@ helm install vault hashicorp/vault \
     --namespace vault \
     -f "${VAULT_OVERRIDE_CONFIG}"
 
+# Install Victoria Metrics
+
+VM_REPO_HELM="https://victoriametrics.github.io/helm-charts/"
+
+helm repo add vm "${VM_REPO_HELM}"
+
+helm repo update
+
+helm search repo vm/
+
+cat <<EOF | helm install vmcluster vm/victoria-metrics-cluster -f -
+vmselect:
+  podAnnotations:
+      prometheus.io/scrape: "true"
+      prometheus.io/port: "8481"
+
+vminsert:
+  podAnnotations:
+      prometheus.io/scrape: "true"
+      prometheus.io/port: "8480"
+
+vmstorage:
+  podAnnotations:
+      prometheus.io/scrape: "true"
+      prometheus.io/port: "8482"
+EOF
+
+# Install vmagent
+
+VM_AGENT_HELM_REPO="https://docs.victoriametrics.com/guides/guide-vmcluster-vmagent-values.yaml"
+
+helm install vmagent vm/victoria-metrics-agent -f "${VM_AGENT_HELM_REPO}"
+
+helm repo update
+
+# Install Grafana
+
+GRAFANA_HELM_REPO="https://grafana.github.io/helm-charts"
+
+helm repo add grafana 
+
+helm repo update
+
+cat <<EOF | helm install my-grafana grafana/grafana -f -
+  datasources:
+    datasources.yaml:
+      apiVersion: 1
+      datasources:
+        - name: victoriametrics
+          type: prometheus
+          orgId: 1
+          url: http://vmcluster-victoria-metrics-cluster-vmselect.default.svc.cluster.local:8481/select/0/prometheus/
+          access: proxy
+          isDefault: true
+          updateIntervalSeconds: 10
+          editable: true
+
+  dashboardProviders:
+   dashboardproviders.yaml:
+     apiVersion: 1
+     providers:
+     - name: 'default'
+       orgId: 1
+       folder: ''
+       type: file
+       disableDeletion: true
+       editable: true
+       options:
+         path: /var/lib/grafana/dashboards/default
+
+  dashboards:
+    default:
+      victoriametrics:
+        gnetId: 11176
+        revision: 18
+        datasource: victoriametrics
+      vmagent:
+        gnetId: 12683
+        revision: 7
+        datasource: victoriametrics
+      kubernetes:
+        gnetId: 14205
+        revision: 1
+        datasource: victoriametrics
+EOF
+
 exit 0;
