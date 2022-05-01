a84b883938ad35747a110e2429b45b4034f2bfec
mstrielnikov <makstr@ukr.net>
2022-05-02 00:38:44 +0300

    Added tf provisionin to aws

---
+++ terraform/aws/vars.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  default     = "eks-cucumbernetes"
+  description = "AWS EKS cluster name"
+}
+
+variable "region" {
+  default     = "eu-central-1"
+  description = "AWS region"
+}
+
+variable "region" {
+  default     = "dev"
+  description = "AWS region"
+}
